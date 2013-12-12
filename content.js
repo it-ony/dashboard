8e6dc631e0e39c5d9995d8198d5df34dfbaccc52
it-ony <github@it-ony.de>
2013-12-12 14:02:14 +0100

    save scroll position

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
  * defines a <meta name="dashboard" /> directive. If so it will setup refresh timers and
  * scroll to defined positions
  */
-(function(chrome, document, window) {
+(function (chrome, document, window) {
 
     /***
      * Check if the page is a dashboard enabled page
@@ -14,11 +14,12 @@
     var isIframe = window.parent !== window && window.parent !== null,
         isDashboard = !isIframe && document.querySelectorAll("meta[name='dashboard']").length > 0,
         frames = [],
+        framesLoading = {},
         iFrameClient = {
-            reload: function() {
+            reload: function () {
                 window.location.reload();
             },
-            addCss: function(data) {
+            addCss: function (data) {
                 var css;
 
                 if (data && data.style) {
@@ -39,23 +40,75 @@
 
                 var head = document.getElementsByTagName("head")[0];
                 head.appendChild(css);
+            },
+            scrollTo: function (position) {
+                window.scrollTo(position.pageXOffset, position.pageYOffset);
             }
         },
         dashboardClient = {
-            ready: function(data, event) {
 
+            _getIFrameForWindow: function (window) {
                 // find frame
                 for (var i = 0; i < frames.length; i++) {
                     var frame = frames[i];
 
-                    if (frame.contentWindow === event.source) {
+                    if (frame.contentWindow === window) {
                         // found the frame
-
                         // send by the iframe, after the content script has been injected
-                        processCssForFrame(frame);
+                        return {
+                            frame: frame,
+                            index: i
+                        };
                     }
                 }
 
+                return null;
+            },
+
+            _getUniqueKey: function () {
+                return "dashboard" + location.pathname;
+            },
+
+            _save: function (key, value) {
+                var uniqueKey = this._getUniqueKey();
+
+                var data = JSON.parse(localStorage.getItem(uniqueKey) || "{}");
+
+                data[key] = value;
+                localStorage.setItem(uniqueKey, JSON.stringify(data));
+            },
+
+            _get: function (key, defaultValue) {
+                var uniqueKey = this._getUniqueKey();
+                var data = JSON.parse(localStorage.getItem(uniqueKey) || "{}");
+
+                return data[key] || defaultValue;
+            },
+
+            ready: function (data, event) {
+                var frame = this._getIFrameForWindow(event.source);
+                frame && processCssForFrame(frame.frame);
+
+                framesLoading[frame.index] = false;
+
+                var scrollPosition = this._get("frame_" + frame.index);
+                if (scrollPosition) {
+                    frame.frame.invoke("scrollTo", scrollPosition);
+                }
+
+            },
+
+            scrolled: function (data, event) {
+
+                if (!window.localStorage) {
+                    return;
+                }
+
+                var frame = this._getIFrameForWindow(event.source);
+                if (frame && framesLoading[frame.index] !== true) {
+                    // save scroll position in localStorage
+                    this._save("frame_" + frame.index, data);
+                }
             }
         },
         client = isIframe ? iFrameClient : dashboardClient;
@@ -63,8 +116,10 @@
     if (isDashboard) {
         frames = Array.prototype.slice.call(document.querySelectorAll("iframe"));
 
-        frames.forEach(function(frame) {
-            frame.invoke = function(method, data) {
+        frames.forEach(function (frame, index) {
+            framesLoading[index] = true;
+
+            frame.invoke = function (method, data) {
                 // TODO: we could also read the origin from the iframe
                 frame.contentWindow.postMessage({
                     method: method,
@@ -75,9 +130,8 @@
             var refreshRate = parseInt(frame.getAttribute("data-refresh"));
             if (!isNaN(refreshRate)) {
                 // a number was given in seconds
-                setInterval(function() {
+                setInterval(function () {
                     frame.invoke("reload");
-
                 }, refreshRate * 1000);
             }
         });
@@ -87,19 +141,35 @@
         window.parent.postMessage({
             method: "ready"
         }, "*");
+
+        window.addEventListener("scroll", function () {
+            window.parent.postMessage({
+                method: "scrolled",
+                data: {
+                    pageXOffset: window.pageXOffset,
+                    pageYOffset: window.pageYOffset
+                }
+            }, "*");
+        }, false);
+
     }
 
-    window.addEventListener("message", function(event) {
+    window.addEventListener("message", function (event) {
 
         // TODO: check origin
         var data = event.data,
             method = client[data.method];
 
+        if (/^_/.test(data.method || "")) {
+            // private method
+            return;
+        }
+
         if (method) {
             // method is available in client
-            method(data.data, event)
+            method.call(client, data.data, event)
         } else {
-            console.log("No handler for method '"+ data.method + "' found!", event);
+            console.log("No handler for method '" + data.method + "' found!", event);
         }
 
     }, false);
